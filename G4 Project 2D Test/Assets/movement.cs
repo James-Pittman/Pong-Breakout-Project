ec8e3139a34232d168b1db60b525c8344ed0c742
Caleb <caleberic6@aol.com>
2021-10-08 17:46:50 -0400

    starting scene stuff

---
+++ G4 Project 2D Test/Assets/movement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    [SerializeField] private float speed = 1f;
+    public Transform obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = gameObject.GetComponent<Transform> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	float h = Input.GetAxis("horip1");
+        Vector3 tempVect = new Vector3(h, 0, 0);
+	tempVect = tempVect.normalized * speed * Time.deltaTime;
+	obj.transform.position += tempVect;
+    }
+}

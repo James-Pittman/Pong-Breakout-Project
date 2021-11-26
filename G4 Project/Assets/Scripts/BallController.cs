24b2d5ab3ea2f280eebc58b277331bd636f2bff5
Connor1736 <connorshrader@gmail.com>
2021-11-26 17:53:37 -0500

    Added documentation for new ball methods

--- G4 Project/Assets/Scripts/BallController.cs
+++ G4 Project/Assets/Scripts/BallController.cs
@@ -14,24 +14,31 @@ public class BallController : MonoBehaviour
     // Thrust applied to the ball.
     public int thrust;
 
-    // Apply a force to the ball based on the balls xForce and yForce.
+    // Apply a force to the ball based on a vector representing the velocity.
     public void ApplyForce(Vector2 vec)
     {
         rb.velocity = Vector2.zero;
         rb.AddForce(vec.normalized * thrust, ForceMode2D.Force);
     }
 
+    // Applies a force to the bell based on the current velocity.
+    // The purpose of this method is to update the velocity when the
+    // thrust is changed.
     public void ApplyForce()
     {
         ApplyForce(rb.velocity);
     }
 
+    // Sets the velocity of the ball based on the speed argument.
+    // For example, if the current thrust is 500 and this function is called
+    // with a value of 1000, the speed of the ball is doubled.
     public void UpdateThrust(int speed)
     {
         thrust = speed;
         ApplyForce();
     }
 
+    // Nudges the ball velocity so that the x component isn't super close to zero.
     public void NudgeX()
     {
         Vector2 currentDirection = rb.velocity.normalized;
@@ -49,6 +56,7 @@ public class BallController : MonoBehaviour
         ApplyForce(newVelocity);
     }
 
+    // Nudges the ball velocity so that the y component isn't super close to zero.
     public void NudgeY()
     {
         Vector2 currentDirection = rb.velocity.normalized;
@@ -81,8 +89,6 @@ public class BallController : MonoBehaviour
             PaddleController paddle = col.gameObject.GetComponent<PaddleController>();
             ownerID = paddle.GetOwnerID();
         }
-
-        
     }
 
     private void OnCollisionExit2D(Collision2D col)
